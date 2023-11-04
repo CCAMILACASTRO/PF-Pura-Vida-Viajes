4e13abe444840e99053147c5bf12785c5827bfe8
camicastro <camicastro.11.94@gmail.com>
2023-11-04 00:33:28 -0300

    first commit

---
+++ BACK/index.js
@@ -0,0 +1,11 @@
+const server = require("./src/server");
+require("dotenv").config();
+const { PORT } = process.env;
+const { conn } = require("./src/db.js");
+
+conn.sync({ alter: true }).then(() => {
+  server.listen(PORT, () => {
+    console.log(`Listen at port ${PORT}`);
+  });
+});
+//
